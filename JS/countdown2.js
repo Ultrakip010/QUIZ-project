8fd83dfce0310b80a8aec8a11adeedaaa957ac91
Jaiden <jaidendor010@gmail.com>
2023-12-21 13:05:34 +0100

    fixing countdown

--- JS/countdown2.js
+++ JS/countdown2.js
@@ -1,28 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Stel de datum en tijd in voor de countdown
-    var countdownDate = new Date("1 mei 2024 00:00:00").getTime();
+// Stel de datum en tijd in voor de aftelling
+var countdownDate2 = new Date("1 may 2024 00:00:00").getTime();
 
-    // Werk de countdown elke 1 seconde bij
-    var countdown = setInterval(function() {
-        // Haal de huidige datum en tijd op
-        var now = new Date().getTime();
+// Werk de aftelling elke 1 seconde bij
+var countdown2 = setInterval(function () {
+    // Haal de huidige datum en tijd op
+    var now = new Date().getTime();
 
-        // Bereken de resterende tijd
-        var distance = countdownDate - now;
+    // Bereken de resterende tijd
+    var distance = countdownDate2 - now;
 
-        // Bereken dagen, uren, minuten en seconden
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Als de aftelling is afgelopen, toon een bericht en stop de update
+    if (distance < 0) {
+        clearInterval(countdown2);
+        document.getElementById("countdown2").innerHTML = "Aftelling is afgelopen!";
+        return;
+    }
 
-        // Toon de countdown
-        document.getElementById("countdown2").innerHTML = days + "d " + hours + "u " + minutes + "m " + seconds + "s ";
+    // Bereken dagen, uren, minuten en seconden
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Als de countdown is afgelopen, toon een bericht
-        if (distance < 0) {
-            clearInterval(countdown);
-            document.getElementById("countdown2").innerHTML = "Countdown is afgelopen!";
-        }
-    }, 1000);
-});
+    // Toon de aftelling
+    document.getElementById("countdown2").innerHTML = days + "d " + hours + "u " + minutes + "m " + seconds + "s ";
+}, 1000);
